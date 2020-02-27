5cfbc03de4bd85e0a436f3d70496fc4084b72832
lewinkoon <lewinkoon+github@gmail.com>
2020-02-27 20:51:34 +0100

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,24 @@
-#!/bin/sh
+#!/bin/env bash
 
-# If a command fails then the deploy stops
+# if a command fails then the deploy stops
 set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+# build the project
+hugo
 
-# Go To Public folder
-cd public
-
-# Add changes to git.
+# push blog
 git add .
+git commit -m "Update"
+git push origin master
 
-# Commit changes.
+# push public
+cd public
+git add .
 msg="rebuilding site $(date)"
 if [ -n "$*" ]; then
 	msg="$*"
 fi
 git commit -m "$msg"
-
-# Push source and build repos.
 git push origin master
